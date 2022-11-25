c5b4f254318d94dbc2b891a851c367409aa55650
musan6363 <msn151336+prog@gmail.com>
2022-11-25 18:59:24 +0900

    feat: アノテーション対象から除外するリストを作成

--- make-reannotate-list.py
+++ make-reannotate-list.py
@@ -10,11 +10,13 @@ class MakeReannotateList:
 
         self.ori_annotated_dir = 'ann/'+self.dataset_name+'/'+self.dataset_version+'/json'
         self.checked_json = 'output/'+self.dataset_name+'/'+self.dataset_version+'/checked_ann_re_do.json'
+        self.checked_notuse_json = 'output/'+self.dataset_name+'/'+self.dataset_version+'/checked_ann_not_use.json'
 
         self.save_path = 'reannotate/'+self.dataset_name+'/'+self.dataset_version
         os.makedirs(self.save_path+'/img', exist_ok=True)
         self.reaanotate_json = self.save_path+'/reannotate.json'
         self.recheck_json = 'output/'+self.dataset_name+'/'+self.dataset_version+'/recheck.json'
+        self.notuse_json = self.save_path+'/notuse.json'
 
     def read_ndjson(self, file_path):
         with open(file_path, 'r') as f:
@@ -58,6 +60,19 @@ class MakeReannotateList:
                 self.recheck_dict.update(_recheck_dst)
                 self.copy_img(_img_name, _ped_token)
 
+    def make_notuse_list(self):
+        self.notuse_list = []
+        _not_use_targets = self.read_ndjson(self.checked_notuse_json)
+        for d in _not_use_targets:
+            for record in d.values():
+                _img_name = record['img_name']
+                _ped_token = record['ped_token']
+                _dst = {
+                    'img_token' : _img_name,
+                    'ped_token' : _ped_token
+                }
+                self.notuse_list.append(_dst)
+
     def copy_img(self, img_token, ped_token):
         shutil.copy2('output/'+self.dataset_name+'/'+self.dataset_version+'/img/'+ped_token+'@'+img_token+'.png', self.save_path+'/img/')
 
@@ -66,10 +81,13 @@ class MakeReannotateList:
             json.dump(self.reannotate_list, f, indent=2, ensure_ascii=False)
         with open(self.recheck_json, 'w', encoding='utf-8') as f:
             json.dump(self.recheck_dict, f, indent=2, ensure_ascii=False)
+        with open(self.notuse_json, 'w', encoding='utf-8') as f:
+            json.dump(self.notuse_list, f, indent=2)
 
     def main(self):
         self.reannotate_target = self.read_ndjson(self.checked_json)
         self.make_list()
+        self.make_notuse_list()
         self.export()
         print(self.reannotate_list, end='\n\n')
         print(self.recheck_dict)
